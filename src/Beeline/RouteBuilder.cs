b1faf8b5697afbdc95737c125178ef51d05a96ac
Johnny Gozde <johnny@jgoz.net>
2011-08-02 18:50:09 -0600

    Minor refactoring.

--- src/Beeline/RouteBuilder.cs
+++ src/Beeline/RouteBuilder.cs
@@ -22,14 +22,35 @@
 			if (actionMethod == null)
 				throw new ArgumentNullException("actionMethod");
 
+			var routeAttributes = actionMethod.GetRouteAttributes();
+
+			if (!routeAttributes.Any() || routeAttributes.Count() > 1)
+				throw new ArgumentException("Expected exactly 1 RouteAttribute on action method " + actionMethod.Name, "actionMethod");
+
 			_actionMethod = actionMethod;
 
+			InitializeMetaData();
+			InitializeRouteData(routeAttributes.Single());
+		}
+
+		public String ActionName { get; private set; }
+		public String ControllerName { get; private set; }
+
+		public String Name { get; private set; }
+		public HttpVerbs Method { get; private set; }
+		public String Url { get; private set; }
+		public RouteValueDictionary Defaults { get; private set; }
+		public RouteValueDictionary Constraints { get; private set; }
+
+		private void InitializeMetaData()
+		{
 			ActionName = _actionMethod.Name;
 			ControllerName = _actionMethod.DeclaringType.Name.Replace("Controller", String.Empty);
 			Method = GetHttpMethod(_actionMethod);
+		}
 
-			RouteAttribute routeAttribute = _actionMethod.GetRouteAttributes().Single();
-
+		private void InitializeRouteData(RouteAttribute routeAttribute)
+		{
 			Name = routeAttribute.Name ?? Method + "." + ControllerName + "." + ActionName;
 			Url = routeAttribute.UrlPattern;
 			Defaults = new RouteValueDictionary(routeAttribute.Defaults ?? new Object())
@@ -42,15 +63,6 @@
 				{ "isValidMethod", new HttpMethodConstraint(ExpandMethod()) }
 			};
 		}
-
-		public String ActionName { get; private set; }
-		public String ControllerName { get; private set; }
-
-		public String Name { get; private set; }
-		public HttpVerbs Method { get; private set; }
-		public String Url { get; private set; }
-		public RouteValueDictionary Defaults { get; private set; }
-		public RouteValueDictionary Constraints { get; private set; }
 
 		private String[] ExpandMethod()
 		{
