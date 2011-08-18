7d9c25edd3ac67283f1c768b248fdd114a3ef4d8
Johnny Gozde <johnny@jgoz.net>
2011-08-17 20:54:01 -0600

    Add Default and Optional attributes to decorate route parameters.

---
+++ src/Beeline/Routing/DefaultAttribute.cs
@@ -0,0 +1,42 @@
+﻿namespace Beeline.Routing
+{
+	using System;
+
+	/// <summary>
+	/// Adds a default parameter value specification to an MVC action method.
+	/// </summary>
+	/// <remarks>
+	/// A <see cref="DefaultAttribute"/> declaration must be used on a method that also has a
+	/// <see cref="RouteAttribute"/> declaration.
+	/// </remarks>
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+	public class DefaultAttribute : Attribute
+	{
+		private readonly String _name;
+		private readonly Object _value;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DefaultAttribute"/> class.
+		/// </summary>
+		/// <param name="name">The URL parameter name.</param>
+		/// <param name="value">The default value for the URL parameter</param>
+		public DefaultAttribute(String name, Object value)
+		{
+			if (String.IsNullOrWhiteSpace(name))
+				throw new ArgumentException("A valid parameter name was expected.", "name");
+
+			_name = name;
+			_value = value;
+		}
+
+		/// <summary>
+		/// Gets the parameter name for this default value.
+		/// </summary>
+		public String Name { get { return _name; } }
+
+		/// <summary>
+		/// Gets the default value for this parameter.
+		/// </summary>
+		public Object Value { get { return _value; } }
+	}
+}
---
+++ src/Beeline/Routing/OptionalAttribute.cs
@@ -0,0 +1,34 @@
+﻿namespace Beeline.Routing
+{
+	using System;
+
+	/// <summary>
+	/// Sets a URL parameter as optional for an MVC action method.
+	/// </summary>
+	/// <remarks>
+	/// A <see cref="OptionalAttribute"/> declaration must be used on a method that also has a
+	/// <see cref="RouteAttribute"/> declaration.
+	/// </remarks>
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+	public class OptionalAttribute : Attribute
+	{
+		private readonly String _name;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="OptionalAttribute"/> class.
+		/// </summary>
+		/// <param name="name">The URL parameter name.</param>
+		public OptionalAttribute(String name)
+		{
+			if (String.IsNullOrWhiteSpace(name))
+				throw new ArgumentException("A valid parameter name was expected.", "name");
+
+			_name = name;
+		}
+
+		/// <summary>
+		/// Gets the parameter name for this optional value.
+		/// </summary>
+		public String Name { get { return _name; } }
+	}
+}
