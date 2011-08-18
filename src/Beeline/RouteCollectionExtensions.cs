3fa74eaa3deebb9273bd84ea581b567d76b2d960
Johnny Gozde <johnny@jgoz.net>
2011-08-17 20:21:55 -0600

    Add MapRoutesInController extension method.

--- src/Beeline/RouteCollectionExtensions.cs
+++ src/Beeline/RouteCollectionExtensions.cs
@@ -38,21 +38,38 @@
 			if (assembly == null)
 				throw new ArgumentNullException("assembly");
 
-			return FindActionMethods(assembly)
-				.Select(RouteBuilder.FromActionMethod)
-				.Select(r => routeCollection.MapRoute(r))
-				.ToList();
+			return routeCollection.MapActionMethodsToRoutes(FindActionMethods(assembly));
+		}
+
+		/// <summary>
+		/// Finds action methods with <see cref="RouteAttribute"/> declarations in a specified controller type
+		/// and maps the results in a specified route collection.
+		/// </summary>
+		/// <typeparam name="TController">The controller type. Must inherit from <see cref="ControllerBase"/></typeparam>
+		/// <param name="routeCollection">The route collection.</param>
+		/// <returns>A list of <see cref="Route"/> objects added to <paramref name="routeCollection"/>.</returns>
+		public static IList<Route> MapRoutesInController<TController>(this RouteCollection routeCollection) where TController : ControllerBase
+		{
+			return routeCollection.MapActionMethodsToRoutes(typeof(TController).GetMethods().Where(m => m.HasRouteAttributes()));
 		}
 
 		private static IEnumerable<MethodInfo> FindActionMethods(Assembly assembly)
 		{
 			return assembly.GetTypes()
-				.Where(t => typeof(Controller).IsAssignableFrom(t))
+				.Where(t => typeof(ControllerBase).IsAssignableFrom(t))
 				.SelectMany(t => t.GetMethods())
 				.Where(m => m.HasRouteAttributes());
 		}
 
-		private static Route MapRoute(this RouteCollection routeCollection, RouteBuilder routeBuilder)
+		private static IList<Route> MapActionMethodsToRoutes(this RouteCollection routeCollection, IEnumerable<MethodInfo> actionMethods)
+		{
+			return actionMethods
+				.Select(RouteBuilder.FromActionMethod)
+				.Select(r => routeCollection.MapRouteBuilder(r))
+				.ToList();
+		}
+
+		private static Route MapRouteBuilder(this RouteCollection routeCollection, RouteBuilder routeBuilder)
 		{
 			Route route = routeCollection.MapRoute(routeBuilder.Name, routeBuilder.Url);
 
