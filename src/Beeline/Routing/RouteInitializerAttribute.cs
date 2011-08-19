8090acf2e3d6116751c8261755a7edb5ebf50611
Johnny Gozde <johnny@jgoz.net>
2011-08-18 22:58:10 -0600

    Add RouteInitializer attribute.

---
+++ src/Beeline/Routing/RouteInitializerAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace Beeline.Routing
+{
+	using System;
+	using System.Web.Routing;
+
+	/// <summary>
+	/// Indicates that a method will be used during route initialization.
+	/// </summary>
+	/// <remarks>
+	/// A route initializer method must accept a <see cref="RouteCollection"/> as its only parameter.
+	/// </remarks>
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+	public class RouteInitializerAttribute : Attribute
+	{
+	}
+}
